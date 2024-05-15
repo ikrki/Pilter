e44d8cb0d3ceda5f9d073b75b6e68243b3a4d34f
yaoyaolingxian <23212010030@m.fudan.edu.cn>
2024-05-15 12:21:41 +0800

    update

--- excel_reader.py
+++ excel_reader.py
@@ -8,9 +8,9 @@ class Simulator:
         self.name = name
         self.count = count
 
-
 def collect_stats(files: [], title: str):
     dic = {}
+    conf_dict = {}
     for file in files:
         file_path = 'input/' + file
         excel_file = pd.ExcelFile(file_path)
@@ -25,16 +25,23 @@ def collect_stats(files: [], title: str):
             except:
                 print("not exist")
                 continue
-            print(column)
 
-            for item in column:
-                names = re.split("[,，、/&+]", str(item))
+            for i in range(len(column)):
+                item=column[i]
+                type=str(data['类别'][i])
+                if 'RTL' in type:
+                    print("skipped "+type)
+                    continue
+                names = re.split("[,，、/&+]", str(item)) if not title=="公司/学校" else [str(item)]
                 for name in names:
                     if len(name) == 0: continue
-                    # print(name)
                     if name[-1] == '2' or name[-1] == '3':
                         name = name[:-1]
                     s = name.lower().replace(' ', '').replace('-', '')
+
+                    if not sheet in conf_dict: conf_dict[sheet] = 0
+                    if not s=='nan': conf_dict[sheet] += 1
+
                     if s in dic:
                         dic[s].count += 1
                     else:
@@ -43,14 +50,18 @@ def collect_stats(files: [], title: str):
     sorted_dict = sorted(dic.items(), key=lambda x: x[1].count, reverse=True)
     sum = 0
     for key, value in sorted_dict:
+        if key == 'nan': continue
         sum += value.count
+
     print(f"Total: {sum}")
     if not os.path.exists('result'): os.makedirs('result')
     output_file = f'result/{title.replace('/', '-')}.txt'
     with open(output_file, "w", encoding='utf-8') as file:
-        # Iterate over the sorted dictionary
         for key, value in sorted_dict:
-            # Write each key-value pair to the file in the specified format
+            if key == 'nan': continue
             file.write(f"{value.name} {value.count}\n")
-            # print(f"{value.name} : {value.count}")
-        file.write(f"Total: {sum}")
+        file.write(f"Total: {sum}\n")
+        if not title=='公司/学校':return
+        for key, value in conf_dict.items():
+            file.write(f"{key} {value}\n")
+
--- main.py
+++ main.py
@@ -13,6 +13,12 @@ ispass2019 = "https://dblp.uni-trier.de/db/conf/ispass/ispass2019.html"
 iiswc2019 = "https://dblp.uni-trier.de/db/conf/iiswc/iiswc2019.html"
 iccad2019 = "https://dblp.uni-trier.de/db/conf/iccad/iccad2019.html"
 
+asplos2024 = ["https://dblp.uni-trier.de/db/conf/asplos/asplos2024-1.html",
+              "https://dblp.uni-trier.de/db/conf/asplos/asplos2024-2.html",
+              "https://dblp.uni-trier.de/db/conf/asplos/asplos2024-3.html"]
+
+hpca2024 = "https://dblp.uni-trier.de/db/conf/hpca/hpca2024.html"
+
 keyword = "simulator"
 
 
@@ -37,13 +43,17 @@ ieee = [
 ]
 
 files = ["模拟2019.xlsx", "模拟2020.xlsx", "模拟2021.xlsx", "模拟2022.xlsx", "模拟2023.xlsx"]
-titles = ["类别", "使用工具", "国家", "公司", "公司/学校"]
+titles = ["使用工具",
+          "公司",
+          "公司/学校"
+          ]
 
 if __name__ == '__main__':
     #############Usage:###############
-    # pdf_reader.get_pdf_files(isca2019,"isca2019")
-    # pdf_reader.read_pdfs("isca2019",keyword)
-    web_reader.read_papers(hpca2019, keyword, "hpca2019",2)
-    # for title in titles:
-    #     excel_reader.collect_stats(files,title)
-    # excel_reader.collect_stats([files[0]],titles[2])
+    # pdf_reader.get_pdf_files(asplos2024,"asplos2024")
+    # pdf_reader.read_pdfs("asplos2024",keyword)
+    # web_reader.read_papers(hpca2024, keyword, "hpca2024")
+    for title in titles:
+        excel_reader.collect_stats(files,title)
+    # excel_reader.collect_stats(files,titles[4])
+    pass
